8c2e86fb1c4809627000aa90b35a0d48394fed47
erickajb88 <erickajb88>
2014-04-18 22:01:11 -0600

    Cambios de css y de menu y vistas

--- application/views/admin_view.php
+++ application/views/admin_view.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<link rel="stylesheet" type="text/css" href="assets/index.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/index.css" />
 	<meta charset="utf-8">
+	
 	<title>Blog</title>	
 </head>
 <body>
 
 	<?php include('menu.php');?>
+
 	<?=form_open(base_url().'comment/false_status/')?>
 
 	<?php
 		if(!empty($comments)){
-			echo '<h2>Comment disable</h2>';
+			echo '<h2>All Comments disabled</h2>';
 			foreach ($comments as $key => $comment)        
 				if($comment['status'] == 'false'){
-					echo '<h2> CODIGO: '.$comment['permalink'].'</h2>'.'<br />'.			
-					' Author: '.$comment['author'].
-					$comment['comment'].'<br />'.
+					echo '<h2> Post: '.$comment['permalink'].'</h2>'.			
+					'<h4>Author: '.$comment['author'].'</h4>' .'<br />'.
+					' Comment: '.$comment['comment'].'<br />'.
 					'<br />'.$comment['date'] . '<hr/>';}	
 				if($comment['status'] == 'true'){			
 					
 				}								
 		}
 		else
-			echo '<h2>No Comments disables!</h2>';								
-	
+			echo '<h2>No Comments disabled!</h2>';	
 	?>
+</body>
--- application/views/menu.php
+++ application/views/menu.php
@@ -1,15 +1,20 @@
 <script type="text/javascript" src="<?=base_url()?>js/moment.js"></script>
 
 <?php 
-	if ($this->session->userdata('is_logged_in'))
-		echo 'Hello, '.$this->session->userdata('name').' ('. anchor(base_url()."users/logout/", "logout") .') | ';
-	
-	else
-		echo anchor(base_url().'users/signin/','Admin').' | ';
-	echo anchor(base_url().'blog/entry/', 'New Entry');
-	echo ' | ';
-	echo anchor(base_url(), 'All Entries');
-	echo ' | ';
-	echo anchor(base_url().'comment/false_status/', 'Admin View');
-	echo '<hr />';
+	if ($this->session->userdata('is_logged_in')){
+
+		echo 'Hello, '.$this->session->userdata('name').' ('. anchor(base_url()."users/logout/","logout") .') | ';
+		echo anchor(base_url(), 'All Entries');	
+		echo ' | ';
+		echo anchor(base_url().'blog/entry/', 'New Entry');
+		echo ' | ';
+		echo anchor(base_url().'comment/false_status/', 'Comments disabled');
+		echo ' | ';
+		echo anchor(base_url().'comment/false_status/', 'Maintenance of comments');
+	}
+	else{
+		echo anchor(base_url().'users/signin/','Admin').' | ';	
+		echo anchor(base_url(), 'All Entries');		
+		echo '<hr />';
+	}		
 ?>
